359c800ce9420e20a7f6f6c254032fc6be84586d
LalbaAnthony <anthony.lalba@limayrac.fr>
2023-12-18 23:04:16 +0100

    Add icons

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,6 +1,18 @@
 <template>
-  <header>
-    <div v-if="!mobile">
+  <header v-if="!mobile">
+    <router-link to="/">
+      <h1 class="main-title">Borrèze</h1>
+    </router-link>
+    <img class="menu-image" src="/img/borreze-header-cropped.jpg" alt="Image de fond de Borrèze">
+    <div class="menu-list">
+      <router-link v-for="item in menu" :key="item.slug" :to="item.path">
+        <component class="menu-icon" :is="getComponent(item.icon)" />
+        {{ item.name }}
+      </router-link>
+    </div>
+  </header>
+  <header v-else>
+    <div>
       hey
     </div>
   </header>
@@ -9,13 +21,11 @@
 <script setup>
 import { ref } from 'vue'
 import { isMobile } from '../helpers/helpers.js'
-
-defineProps({
-  alerteMessage: {
-    type: String,
-    required: false
-  }
-})
+import IconHouse from './icons/IconHouse.vue'
+import IconCalendar from './icons/IconCalendar.vue'
+import IconBuilding from './icons/IconBuilding.vue'
+import IconInfoCircle from './icons/IconInfoCircle.vue'
+import IconEnvelope from './icons/IconEnvelope.vue'
 
 const mobile = isMobile()
 const menu = ref([
@@ -23,33 +33,50 @@ const menu = ref([
     name: 'Accueil',
     slug: 'accueil',
     path: '/',
-    icon: '',
+    icon: 'IconHouse',
   },
   {
     name: 'Actualités',
     slug: 'actualites',
     path: '/actualites',
-    icon: '',
+    icon: 'IconCalendar',
   },
   {
-    name: 'Salles de fêtes',
+    name: 'Salles des fêtes',
     slug: 'salles-des-fetes',
     path: '/salles-des-fetes',
-    icon: '',
+    icon: 'IconBuilding',
   },
   {
     name: 'À propos',
     slug: 'a-propos',
     path: '/a-propos',
-    icon: '',
+    icon: 'IconInfoCircle',
   },
   {
     name: 'Contact',
     slug: 'contact',
     path: '/contact',
-    icon: '',
+    icon: 'IconEnvelope',
   },
 ])
+
+const getComponent = (icon) => {
+  switch (icon) {
+    case 'IconHouse':
+      return IconHouse;
+    case 'IconCalendar':
+      return IconCalendar;
+    case 'IconBuilding':
+      return IconBuilding;
+    case 'IconInfoCircle':
+      return IconInfoCircle;
+    case 'IconEnvelope':
+      return IconEnvelope;
+    default:
+      return null;
+  }
+};
 
 // function toggleSidePanel() {
 //   sidePanelOpened.value = !sidePanelOpened.value;
@@ -63,8 +90,86 @@ const menu = ref([
 
 <style scoped>
 header {
-  color: var(--vt-c-white);
-  padding: 0.5rem;
+  display: flex;
+  flex-direction: column;
+}
+
+h1.main-title {
+  color: var(--light);
+  position: absolute;
+  top: 0;
+  left: 0;
+  font-family: 'Rethink Sans', 'Roboto';
+  transform: translate(40%, 30%);
+  font-size: 80px;
+  z-index: 1;
+}
+
+/* h1.main-title::before {
+  content: 'Commune';
+  font-size: 13px;
+  position: absolute;
+  top: 54px;
+  left: -40px;
+  rotate: -90deg;
+} */
+
+h1.main-title:after {
+  content: "";
+  display: block;
+  width: 120px;
+  height: 5px;
+  background-color: var(--primary);
+  margin: 0.5rem auto;
+  transform: translate(0, -30px);
+  opacity: 1;
+  animation-name: slideAndOpacity;
+  animation-duration: 0.5s;
+}
+
+@keyframes slideAndOpacity {
+  0% {
+    opacity: 0;
+    transform: translate(-300px, -30px);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translate(0px, -30px);
+  }
+}
+
+img.menu-image {
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+  object-position: center;
+  filter: brightness(50%)
+}
+
+.menu-list {
+  background-color: var(--primary);
+  color: var(--light);
+  display: flex;
+  justify-content: center;
   align-items: center;
+}
+
+.menu-list>* {
+  background-color: var(--primary);
+  color: var(--light);
+  padding: 0.5rem 1.5rem;
+}
+
+.menu-list>*:hover {
+  background-color: var(--light);
+  color: var(--primary);
+  transition: all 0.2s ease-in-out;
+}
+
+.menu-icon {
+  position: relative;
+  top: 2px;
+  right: 5px;
 }
 </style>
