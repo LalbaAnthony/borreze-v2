e9693507d26b8ee13ea5100977480ec3fb815e54
LalbaAnthony <anthony.lalba@limayrac.fr>
2023-12-19 09:59:48 +0100

    inté mobile header

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,22 +1,36 @@
 <template>
-  <header v-if="!mobile">
+  <header>
     <router-link to="/">
       <h1 class="main-title">Borrèze</h1>
     </router-link>
-    <div class="quick-access">
-      LIENS PRATIQUES
+    <div v-if="!mobile" class="quick-access">
+      <div class="quick-access-list">
+        <a href="tel:+33553288338">
+          <IconTelephone class="quick-acces-icon icon-offset" />
+          <p>Appeler</p>
+        </a>
+        <a href="mailto:mairie.borreze@wanadoo.fr">
+          <IconEnvelope class="quick-acces-icon icon-offset" />
+          <p>E-mail</p>
+        </a>
+        <a href="https://www.facebook.com/MairiedeBorreze/" target="_blank" rel="noopener noreferrer">
+          <IconFacebook class="quick-acces-icon icon-offset" />
+          <p>Facebook</p>
+        </a>
+      </div>
     </div>
-    <img class="menu-image" src="/img/borreze-header-cropped.jpg" alt="Image de fond de Borrèze">
+    <img class="header-image" src="/img/borreze-header-cropped.jpg" alt="Image de fond de Borrèze">
+    <!-- <div v-if="mobile"> -->
+      
+
+
+
     <div class="menu-list">
-      <router-link v-for="item in menu" :key="item.slug" :to="item.path">
-        <component class="menu-icon" :is="getComponent(item.icon)" />
+      <router-link v-for="item in menu" :key="item.slug" :to="item.path"
+        :class="route.name === item.slug ? 'selected' : ''">
+        <component class="icon-offset" :is="getComponent(item.icon)" />
         {{ item.name }}
       </router-link>
-    </div>
-  </header>
-  <header v-else>
-    <div>
-      hey
     </div>
   </header>
 </template>
@@ -29,6 +43,8 @@ import IconCalendar from './icons/IconCalendar.vue'
 import IconBuilding from './icons/IconBuilding.vue'
 import IconInfoCircle from './icons/IconInfoCircle.vue'
 import IconEnvelope from './icons/IconEnvelope.vue'
+import IconTelephone from './icons/IconTelephone.vue'
+import IconFacebook from './icons/IconFacebook.vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
@@ -99,25 +115,54 @@ header {
   flex-direction: column;
 }
 
+/* DESKTOP */
+@media (min-width: 767px) {
+  h1.main-title {
+    font-size: 80px;
+  }
+
+  h1.main-title:after {
+  transform: translate(0, -30px);
+}
+}
+
+/* MOBILE */
+@media (max-width: 767px) {
+  h1.main-title {
+    font-size: 60px;
+  }
+
+  h1.main-title:after {
+    transform: translate(0, -20px);
+  }
+
+  .quick-access {
+    display: none;
+  }
+
+  .menu-list {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .menu-list>* {
+    /* padding: 0.5rem 0; */
+    width: 100%;
+  }
+  .menu-list>*:last-child {
+border-bottom: solid 2px var(--primary);
+  }
+}
+
 h1.main-title {
   color: var(--light);
   position: absolute;
   top: 40px;
   left: 10%;
   font-family: 'Rethink Sans', 'Roboto';
-  font-size: 80px;
   z-index: 1;
   text-shadow: 1px 1px 2px black;
 }
-
-/* h1.main-title::before {
-  content: 'Commune';
-  font-size: 13px;
-  position: absolute;
-  top: 54px;
-  left: -40px;
-  rotate: -90deg;
-} */
 
 h1.main-title:after {
   content: "";
@@ -126,7 +171,6 @@ h1.main-title:after {
   height: 5px;
   background-color: var(--primary);
   margin: 0.5rem auto;
-  transform: translate(0, -30px);
   opacity: 1;
   animation-name: slideAndOpacity;
   animation-duration: 0.5s;
@@ -156,7 +200,30 @@ h1.main-title:after {
   z-index: 1;
 }
 
-img.menu-image {
+.quick-access-list {
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  grid-gap: 15px;
+}
+
+.quick-access-list>* {
+  color: var(--light);
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  gap: 0.25rem;
+}
+
+.quick-access-list>*:hover {
+  scale: 1.1;
+  transition: all 0.2s ease-in-out;
+}
+
+.quick-acces-icon {
+  margin: 0 auto;
+}
+
+img.header-image {
   width: 100%;
   height: 200px;
   object-fit: cover;
@@ -175,16 +242,17 @@ img.menu-image {
 .menu-list>* {
   background-color: var(--primary);
   color: var(--light);
-  padding: 0.5rem 2rem;
+  padding: 0.5rem 5%;
 }
 
-.menu-list>*:hover {
+.menu-list>*:hover,
+.menu-list>*.selected {
   background-color: var(--light);
   color: var(--primary);
   transition: all 0.2s ease-in-out;
 }
 
-.menu-icon {
+.icon-offset {
   position: relative;
   top: 2px;
   right: 5px;
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'homepage',
+      name: 'accueil',
       component: () => import('../pages/HomePage.vue'),
     },
     {
