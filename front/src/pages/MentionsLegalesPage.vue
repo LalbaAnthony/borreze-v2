d166b09853e562c6619481e28e91c79e7672da90
LalbaAnthony <anthony.lalba@limayrac.fr>
2023-12-25 23:15:17 +0100

    Mentions légales + plan du site

--- front/src/pages/MentionsLegalesPage.vue
+++ front/src/pages/MentionsLegalesPage.vue
@@ -1,5 +1,87 @@
 <template>
-  <div>WIP</div>
+  <h2 class="page-title">Mentions légales</h2>
+
+  <section>
+    <h3 class="section-title">Éditeur du site</h3>
+    <p>Vous êtes sur le site Internet : <a href="https://www.borreze.com" class="link">https://www.borreze.com</a></p>
+    <p>Le site « borreze.com » ainsi que tous ses composants sont la propriété de la commune de Borrèze.</p><br>
+    <p>Commune de Borrèze</p>
+    <p>Adresse : Mairie de Borrèze, Le Bourg, 24590 Borrèze </p>
+    <p>Téléphone : <a class="link" href="tel:+33553288338">05 53 28 83 38</a></p>
+    <p>Email : <a class="link" href="mailto:mairie.borreze@wanadoo.fr">mairie.borreze@wanadoo.fr</a></p>
+  </section>
+
+  <section>
+    <h3 class="section-title">Responsabilités</h3>
+    <p>La Commune de Borrèze s'efforce de fournir des informations exactes et à jour sur son site, mais ne peut garantir
+      l'exactitude et l'exhaustivité des informations fournies.</p>
+    <p>En cas d'erreur ou d'omission, veuillez nous contacter pour rectification.</p>
+    <p>Le contenu du site respecte les lois en vigueur et notamment :</p>
+    <p>La loi sur la liberté de la presse de sorte qu'aucun écrit, image ou son à caractère diffamatoire, raciste ou
+      révisionniste ne sera mis en ligne ;</p>
+    <p>Conformément à l'article 227-24 du Code pénal relatif à la protection des mineurs, aucun message à caractère
+      violent,
+      pornographique ou portant gravement atteinte à la dignité humaine susceptible d'être vu ou perçu par un mineur ne
+      sera
+      mis en ligne ;</p>
+    <p>Conformément à la loi LCEN du 22 juin 2004, un droit de réponse est ouvert à toute personne nommée ou désignée sur
+      le
+      site. La demande doit être faite au directeur de publication au plus tard dans les trois mois de la mise à
+      disposition
+      du public du message justifiant cette demande. Cette insertion est gratuite et se fera dans un délai de trois jours
+      de
+      la réception de la demande, mais ne pourra en aucun cas dépasser deux cents lignes.</p>
+  </section>
+
+  <section>
+    <h3 class="section-title">Collecte de données personnelles</h3>
+    <p>Le site de la Commune de Borrèze ne collecte aucune donnée personnelle, ne dispose d'aucun formulaire de collecte,
+      et ne fait usage d'aucun cookie de suivi.</p>
+  </section>
+
+  <section>
+    <h3 class="section-title">Loi informatique, fichiers et libertés et protection des données personnelles</h3>
+    <p>Les utilisateurs du site sont tenus de respecter les dispositions de la loi informatique, fichiers et libertés,
+      dont
+      la violation est passible de sanctions pénales. Ils doivent notamment s'abstenir, s'agissant des informations
+      nominatives auxquelles ils accèdent, de toute collecte, de toute utilisation détournée et, d'une manière générale,
+      de
+      tout acte susceptible de porter atteinte à la vie privée ou à la réputation des personnes.</p>
+
+    <p>La commune de Borrèze ne pourra être tenue pour responsable de tout dommage direct et/ou indirect résultant de
+      l'utilisation
+      faite par le client du site et/ou de toute information qu'il contient.</p>
+
+    <p>Le site borreze.com peut renvoyer l'utilisateur à d'autres sites internet par le biais de liens hypertexte. Ces
+      liens
+      ont
+      été insérés dans le site borreze.com avec l'autorisation des titulaires des sites liés. En tout état de cause, la
+      borreze.com
+      ne saurait être responsable du contenu de ses sites, lesquels relèvent de l'entière responsabilité de leurs
+      titulaires.</p>
+
+    <p>Certains dangers sont inhérents à l'utilisation de l'internet et l'utilisateur est mis en garde quant à la
+      nécessité
+      de prendre en compte les risques potentiels (toute infection par virus d'ordinateur, tout bogue, tout
+      ralentissement,
+      etc…) avant de procéder à la consultation du site borreze.com.</p>
+  </section>
+
+  <section>
+    <h3 class="section-title">Propriété intellectuelle et droit d'auteur</h3>
+    <p>Le contenu de ce site relève de la législation française et internationale sur le droit d'auteur et la propriété
+      intellectuelle.</p>
+
+    <p>La commune de Borrèze conserve tous les droits patrimoniaux et moraux liés aux documents de ce site, sous réserve
+      des
+      droits dont bénéficient les auteurs d'œuvres publiées sur ce site. Ainsi, tous les droits de reproduction, sur
+      supports électroniques ou papier, sont-ils réservés, y compris pour les documents téléchargeables et les
+      représentations iconographiques et/ou photographiques.</p>
+  </section>
+
+  <section>
+    <p>Date de dernière mise à jour : 25 décembre 2023</p>
+  </section>
 </template>
 
 <script setup>
